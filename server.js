1e41af30cab208a70a1a4c4105853a68fcb0665c
Jada Arnett <jada.arnett@gmail.com>
2020-09-30 13:00:37 -0400

    setting environment variable

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const path = require("path");
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
-
-
 // NOTES 
 var notes = [];
+
+
 
 // HTML ROUTES 
 
