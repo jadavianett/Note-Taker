6c22774f546e0e74ec74c3fbf22d05de6d5ccde0
Jada Arnett <jada.arnett@gmail.com>
2020-10-01 14:50:34 -0400

    adding post menthod

--- server.js
+++ server.js
@@ -1,81 +1,71 @@
-
 const express = require("express");
 const path = require("path");
-const db = require("./db/db.json")
+const db = require("./db/db.json");
+const fs = require("fs");
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middleware 
+//middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// HTML ROUTES
 
-// NOTES 
-// var notes = [{
-//     title: "hello",
-//     text: "nope"
-// }];
-
-
-// HTML ROUTES 
-
-// get /notes 
+// get /notes
 // should return the notes.html file
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// get * 
+// get *
 // should return the index.html file
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
 
 //API ROUTES
 
 // get /api/notes
-// should read the db.json file 
-// return all saved notes as JSON 
+// should read the db.json file
+// return all saved notes as JSON
 app.get("/api/notes", function (req, res) {
- res.json(db);
-    
+  res.json(db);
 });
 
-//post /api/notes 
+//post /api/notes
 // should receive a new note to save on the request body
 // add it to the db.json file
-// return the new note to the client 
+// return the new note to the client
 
-app.post("/api/notes", function (req, res)
-{
-    var newNote = req.body;
-
-    console.log(newNote);
-  
-    //fs.readfile, parse data
-    
-    notes.push(newNote);
-  //fs.writefile
-    res.json(newNote);
-
+app.post("/api/notes", function (req, res) {
+  try {
+    var notesList = fs.readFileSync("./db/db.json", "utf8");
+    notesList = JSON.parse(notesList);
+    for (var i = 0; i <= notesList.length; i++) {
+        req.body.id=notesList.length
+    }
+    notesList.push(req.body);
+    notesList = JSON.stringify(notesList);
+    fs.writeFile("./db/db.json", notesList, "utf8", function (err) {
+      if (err) throw err;
+    });
+    res.json(req.body);
+  } catch (err) {
+    throw err;
+  }
 });
 
 // delete /api/notes/:id
-// should receive a query param containing the id of a note to delete 
-    // this means you'll need to find a way to give each note a unique id when its saved
+
+//same as the post except you need a filter
+// should receive a query param containing the id of a note to delete
+// this means you'll need to find a way to give each note a unique id when its saved
 // read all notes from the db.json file, remove the note with the given 'id' property, and rewrite the notes to the db.json file
 
-
-
-
-
-
-
 //starts the server listening
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
